9fa2d7036cd7afdbc6e16cf421f8873df52d6392
Joel Quimper <dieu@jquimperdev.onmicrosoft.com>
2025-02-15 15:09:14 -0500

    ajout de la prise en charge du client QuestionAnswering pour améliorer les réponses du bot Teams

--- agent-api/Bots/TeamsConversationBot.cs
+++ agent-api/Bots/TeamsConversationBot.cs
@@ -1,3 +1,4 @@
+using Azure.AI.Language.QuestionAnswering;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Teams;
 using Microsoft.Bot.Schema;
@@ -6,13 +7,33 @@ namespace AgentApi.Bots
 {
     public class TeamsConversationBot : TeamsActivityHandler
     {
+        private ILogger<TeamsConversationBot> _logger;
+        private readonly QuestionAnsweringClient _questionAnsweringClient;
+
+        public TeamsConversationBot(QuestionAnsweringClient questionAnsweringClient, ILogger<TeamsConversationBot> logger)
+        {
+            _questionAnsweringClient = questionAnsweringClient;
+            _logger = logger;
+        }
         protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
         {
+            _logger.LogInformation("Running TeamsConversationBot.OnMessageActivityAsync");
             turnContext.Activity.RemoveRecipientMention();
             var text = turnContext.Activity.Text.Trim().ToLower();
+            _logger.LogInformation($"Received message: {text}");
 
-            // Exemple d'appel asynchrone pour envoyer une réponse
-            await turnContext.SendActivityAsync(MessageFactory.Text($"Vous avez dit : {text}"), cancellationToken);
+            QuestionAnsweringProject project = new QuestionAnsweringProject("hr-kb-fr", "production");
+            var result = await _questionAnsweringClient.GetAnswersAsync(text, project, new AnswersOptions { Size = 1 });
+
+            // if confidence is higher than 50% send the answer
+            if (result.Value.Answers[0].Confidence > 0.7)
+            {
+                await turnContext.SendActivityAsync(MessageFactory.Text(result.Value.Answers[0].Answer), cancellationToken);
+            }
+            else
+            {
+                await turnContext.SendActivityAsync(MessageFactory.Text("Je ne sais pas répondre à cette question."), cancellationToken);
+            }
         }
     }
 }
--- agent-api/Program.cs
+++ agent-api/Program.cs
@@ -1,6 +1,8 @@
 using AgentApi.Bots;
 using AgentApi.Infrastructure.ApiKey;
 using AgentApi.Infrastructure.Bot;
+using Azure.AI.Language.QuestionAnswering;
+using Azure.Identity;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Integration.AspNet.Core;
 using Microsoft.Bot.Connector.Authentication;
@@ -27,6 +29,18 @@ builder.Services.AddSingleton<IBotFrameworkHttpAdapter, AdapterWithErrorHandler>
 
 // Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
 builder.Services.AddTransient<IBot, TeamsConversationBot>();
+
+// Add the QuestionAnsweringClient as a singleton
+builder.Services.AddSingleton<QuestionAnsweringClient>(q =>
+{
+    var baseUrl = builder.Configuration.GetValue<string>("AzureAI:QuestionAnsweringBaseUrl");
+    if (string.IsNullOrEmpty(baseUrl))
+    {
+        throw new ArgumentNullException(nameof(baseUrl), "The AzureAI:QuestionAnsweringBaseUrl configuration value is missing or empty.");
+    }
+
+    return new QuestionAnsweringClient(new Uri(baseUrl), new DefaultAzureCredential());
+});
 
 var app = builder.Build();
 
